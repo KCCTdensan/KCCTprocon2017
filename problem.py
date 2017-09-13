806f23a99ff079d55f032098bd59dcae4e13bbbe
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2017-09-13 17:42:08 +0900

    関数追加

--- problem.py
+++ problem.py
@@ -2,3 +2,5 @@ class problem:
         def __init__(self,pieces,frame):
                 self.pieces=pieces
                 self.frame=frame
+        def serach_360deg_corner(self):
+                pass
