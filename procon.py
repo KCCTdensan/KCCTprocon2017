613f1bf36be62c254be7771e982aa0e228f987ee
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2017-09-21 23:16:51 +0900

    ”aiueo”→”True”

--- procon.py
+++ procon.py
@@ -13,4 +13,4 @@ root_problem=problem(*QR.read_QR())
 gui=GUI.GUI(root_problem)
 test_piece=piece(numpy.array([(0.0,0.0),(1.0,1.0),(2.0,2.0),(3.0,3.0)]))
 if test_piece.is_on_grid():
-    print("aiueo")
+    print("True")
