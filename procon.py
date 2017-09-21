89631c52b2c423e9e8e3997b5e5a33a614286cc3
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2017-09-21 23:03:51 +0900

    デバッグピースの実装

--- procon.py
+++ procon.py
@@ -6,5 +6,11 @@ import GUI
 from problem import problem
 
 import QR
+
+from piece import piece
+
 root_problem=problem(*QR.read_QR())
 gui=GUI.GUI(root_problem)
+test_piece=piece(numpy.array([(0.0,0.0),(1.0,1.0),(2.0,2.0),(3.0,3.0)]))
+if test_piece.is_on_grid():
+    print("aiueo")
