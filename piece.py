05efe0262ff1b5a9351d69cc439404826217f2d3
sabinosabi <sabinosabi@users.noreply.github.com>
2017-09-24 19:32:35 +0900

    is_overlapped修正
    self_vertexとanother_vertexを頂点の番号としての処理に修正
    図形の内包判定について自分なりに実装
    anotherピースが辺を引いていく方向とは逆向きにほんの僅かだけずらした点を見て、selfピースの辺のベクトルとselfの点からずらした点とのベクトルの外積が>0であればその点はselfピースの辺の右側（つまり内側）となるのを利用して、全てのselfピースの辺とanotherの頂点からずらした点で外積をして判定。
    anotherピースがselfピースより外側で結合している場合は外積が<0で内側になっていると思う（この部分はanotherピースがselfピースより外という判定が甘いかもしれない）

--- piece.py
+++ piece.py
@@ -24,15 +24,17 @@ class piece:
                 pass
 
         def is_overlapped(self,another,self_vertex1,self_vertex2,another_vertex1,another_vertex2):
-            A = self_vertex1 - another_vertex1
-            B = self_vertex2 - another_vertex2
+            A = self.vertexes[self_vertex1] - another.vertexes[another_vertex1]
+            B = self.vertexes[self_vertex2] - another.vertexes[another_vertex2]
             if numpy.allclose(A,B):
-                vertex_increment = self_vertex1 - another_vertex1
+                vertex_increment = A
             else:
-                vertex_increment = self_vertex1 - another_vertex2
+                vertex_increment = self.vertexes[self_vertex1] - another.vertexes[another_vertex2]
 
             another_piece = another
             self_piece = self
+
+            another_to_self = False
 
             if (vertex_increment >= 0).all():
                 for i,vertex in enumerate(another.vertexes):
@@ -41,6 +43,13 @@ class piece:
                 vertex_increment *= -1
                 for i,vertex in enumerate(self.vertexes):
                     self_piece.vertexes[i] += vertex_increment
+                    another_to_self = True
+            print("増分=",vertex_increment)
+
+            print("自ピース=",self_piece.vertexes)
+            print("他ピース=",another_piece.vertexes)
+
+            check_isinside = []
 
             for i,self_search_vertex1 in enumerate(self_piece.vertexes):
                 if not numpy.allclose(self_search_vertex1,self_piece.vertexes[-1]):
@@ -57,6 +66,15 @@ class piece:
                     t_c = (another_search_vertex1[0] - another_search_vertex2[0]) * (self_search_vertex1[1] - another_search_vertex1[1]) + (another_search_vertex1[1] - another_search_vertex2[1]) * (another_search_vertex1[0] - self_search_vertex1[0])
                     t_d = (another_search_vertex1[0] - another_search_vertex2[0]) * (self_search_vertex2[1] - another_search_vertex1[1]) + (another_search_vertex1[1] - another_search_vertex2[1]) * (another_search_vertex1[0] - self_search_vertex2[0])
                     if t_a * t_b < 0 and t_c * t_d < 0: return True 
+                    another_check_vertex = another_search_vertex2 - 0.01 * (another_search_vertex2 - another_search_vertex1) / numpy.linalg.norm(another_search_vertex2 - another_search_vertex1)
+                    cross = numpy.cross(self_search_vertex2 - self_search_vertex1,another_check_vertex - self_search_vertex1)
+                    if cross > 0 and not another_to_self: check_isinside.append(k)
+                    elif cross < 0 and another_to_self: check_isinside.append(k)
+            check_isinside.sort()
+            print("checkinside=",check_isinside)
+            for i,vartex in enumerate(another_piece.vertexes):
+                if check_isinside.count(i) == len(self_piece.vertexes) - 1:
+                    return True
             return False
 
         def merge(self):
