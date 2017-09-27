946cdaf4544501f55c27dddb328bd0590fa8a77d
sabinosabi <sabinosabi@users.noreply.github.com>
2017-09-27 13:16:03 +0900

    is_overlapped修正
    プッシュし忘れていたので今更ながら
    枠の外側にピースの頂点があれば重なっているとみなすように変更

--- piece.py
+++ piece.py
@@ -34,8 +34,6 @@ class piece:
             another_piece = another
             self_piece = self
 
-            another_to_self = False
-
             if (vertex_increment >= 0).all():
                 for i,vertex in enumerate(another.vertexes):
                     another_piece.vertexes[i] += vertex_increment
@@ -43,7 +41,6 @@ class piece:
                 vertex_increment *= -1
                 for i,vertex in enumerate(self.vertexes):
                     self_piece.vertexes[i] += vertex_increment
-                    another_to_self = True
             print("増分=",vertex_increment)
 
             print("自ピース=",self_piece.vertexes)
@@ -66,15 +63,8 @@ class piece:
                     t_c = (another_search_vertex1[0] - another_search_vertex2[0]) * (self_search_vertex1[1] - another_search_vertex1[1]) + (another_search_vertex1[1] - another_search_vertex2[1]) * (another_search_vertex1[0] - self_search_vertex1[0])
                     t_d = (another_search_vertex1[0] - another_search_vertex2[0]) * (self_search_vertex2[1] - another_search_vertex1[1]) + (another_search_vertex1[1] - another_search_vertex2[1]) * (another_search_vertex1[0] - self_search_vertex2[0])
                     if t_a * t_b < 0 and t_c * t_d < 0: return True 
-                    another_check_vertex = another_search_vertex2 - 0.01 * (another_search_vertex2 - another_search_vertex1) / numpy.linalg.norm(another_search_vertex2 - another_search_vertex1)
-                    cross = numpy.cross(self_search_vertex2 - self_search_vertex1,another_check_vertex - self_search_vertex1)
-                    if cross > 0 and not another_to_self: check_isinside.append(k)
-                    elif cross < 0 and another_to_self: check_isinside.append(k)
-            check_isinside.sort()
-            print("checkinside=",check_isinside)
-            for i,vartex in enumerate(another_piece.vertexes):
-                if check_isinside.count(i) == len(self_piece.vertexes) - 1:
-                    return True
+                    cross = numpy.cross(self_search_vertex2 - self_search_vertex1,another_search_vertex1 - self_search_vertex1)
+                    if cross < 0 : return True
             return False
 
         def merge(self):
