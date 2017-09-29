d2d9fa047fa3f72ecaa7c0fbdb238bf1aafab3aa
縦椅子鉋 <TateIsuKanna@users.noreply.github.com>
2017-09-29 14:29:44 +0900

    無駄な処理の削除

--- piece.py
+++ piece.py
@@ -3,22 +3,14 @@ import numpy
 class piece:
     def __init__(self,vertexes):
         self.vertexes = vertexes
-        self.length = []
-        for i,vertex in enumerate(vertexes):
-            self.length.append(numpy.linalg.norm(vertexes[(i + 1) % len(vertexes)] - vertex))
-        print("length",self.length)
         self.angles = []
-        self.inner_products = []
         for i,vertex in enumerate(vertexes):
             vec_front = vertexes[(i + 1) % len(vertexes)] - vertex
             vec_back = vertex - vertexes[(i - 1 + len(vertexes)) % len(vertexes)]
             inner_product = numpy.dot(vec_front,vec_back)
             angle = math.acos(inner_product / (numpy.linalg.norm(vec_front) * numpy.linalg.norm(vec_back)))
-            self.inner_products.append(inner_product)
             self.angles.append(angle)
-        print("角度 ラジアン",self.angles)
         print("角度 度",[numpy.rad2deg(i) for i in self.angles])
-        print("内積",self.inner_products)
 
     def is_on_grid(self):
         return numpy.allclose(self.vertexes,numpy.floor(self.vertexes))
