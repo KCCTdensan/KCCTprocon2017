64f6009f2280900991ae6dc58fb792e46c5027d6
kumataro <r116309@g.kobe-kosen.ac.jp>
2017-09-30 19:02:29 +0900

    def calc_eval_value(self,piece_vartexes) ピースの場所に対する評価値を返します。評価値は、枠の各頂点の座標とピースの各頂点の座標が重複する数で表されます。 引数:ピースの各頂点の座標の配列 戻り値:評価値

--- problem.py
+++ problem.py
@@ -1,6 +1,11 @@
+import numpy as np
+
 class problem:
         def __init__(self,pieces,frame):
                 self.pieces=pieces
                 self.frame=frame
         def serach_360deg_corner(self):
-                print("kumataro TEST")
+                pass
+
+        def calc_eval_value(self,piece_vertexes):
+                return len(set(map(tuple(self.frame)))&map(tuple(piece_vertexes)))
