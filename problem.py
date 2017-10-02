5213b4b088965ff96f56aa8dddc8a6de3a1de6a6
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2017-10-02 14:51:58 +0900

    dfs_corner()の擬似コードの実装

--- problem.py
+++ problem.py
@@ -4,3 +4,18 @@ class problem:
         self.frame = frame
     def serach_360deg_corner(self):
         print("kumataro TEST")
+
+    def dfs_corner(self,frame):
+        pass
+		#結合可能なケースを探す
+		#if ケースが無い:
+		#	if パズル完成:
+		#		return true
+		#	else
+		#		return false
+		#評価値をつける
+		#評価値順にソート
+		#for i in ケースの数
+		#	if dfs(self,self.frame.marge())=true
+		#		return true;
+		#return false
