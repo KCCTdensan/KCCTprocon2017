8733e94bc0a635e376b3d4dda498a97033e74dfa
tateisukanna <tateisukanna@users.noreply.github.com>
2017-10-04 17:19:44 +0900

    スペルの変更 merge_pieces()の実装

--- problem.py
+++ problem.py
@@ -22,7 +22,7 @@ class problem:
         """
         pass
 
-    def marge_pieces(self,pieces):
+    def merge_pieces(self,pieces):
         """枠にはまるピースのリストを枠と結合し、リストにまとめて返します(frame)。
 
         pieces: 
@@ -30,7 +30,11 @@ class problem:
         戻り値:
             pieceのリスト(frame)
         """
-        pass
+        frame=[]
+        for P in pieces:
+            frame +=[self.frame.merge(P)]
+
+        return frame
 
     def dfs_corner(self,frame,depth):
         """角の深さ優先探索を行います
@@ -42,7 +46,7 @@ class problem:
         戻り値: bool
         """
 
-        match_frames=marge_pieces(sorting(search_match_pieces()))#結合可能なピースを評価値順にソートし、枠と結合する
+        match_frames=merge_pieces(sorting(search_match_pieces()))#結合可能なピースを評価値順にソートし、枠と結合する
 		#if 結合可能なピースが無い:
 		#	if パズル完成:
         #       GUI更新
