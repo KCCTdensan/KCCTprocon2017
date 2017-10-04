9625bcaf25a16df02d63204059e0b200556e604c
kumataro4160 <r116309@g.kobe-kosen.ac.jp>
2017-10-04 16:15:24 +0900

    problem.calc_eval_value関数の説明を追加 以下説明:
    結合するピースの位置に対する評価値を返します。
    piece_vertexes:
        ピースの各頂点の座標の配列
    
     評価値は、枠の各頂点の座標とピースの各頂点の座標が重複する数で表されます。
    各頂点の座標は枠に対して正確な位置である必要があります。

--- problem.py
+++ problem.py
@@ -8,4 +8,13 @@ class problem:
                 pass
 
         def calc_eval_value(self,piece_vertexes):
+                """
+                結合するピースの位置に対する評価値を返します。
+
+                piece_vertexes:
+                   ピースの各頂点の座標の配列
+
+                評価値は、枠の各頂点の座標とピースの各頂点の座標が重複する数で表されます。
+                各頂点の座標は枠に対して正確な位置である必要があります。
+                """
                 return len(set(map(tuple,self.frame.vertexes))&set(map(tuple,piece_vertexes)))
