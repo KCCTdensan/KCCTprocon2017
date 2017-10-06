bb254aac500957c4bc988810eca7e722984d5d86
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2017-10-07 01:27:33 +0900

    大枠は完成．調整が必要!

--- GUI.py
+++ GUI.py
@@ -8,11 +8,11 @@ class GUI():
 
         problem: problem
         """
+        self.problem=problem #TODO:問題になるかも
         zoom=self.zoom_scale.get()
-        self.history_depth.configure(to=(len(problem.merge_history)-1))
-        self.draw_piece(problem.merge_history[self.history_depth.get()][0],zoom,5,50*zoom)
+        self.history_depth.configure(to=len(problem.merge_history)-1)
+        self.draw_piece(problem.merge_history[self.history_depth.get()][0].frame,zoom,5,50*zoom)
         self.draw_piece(problem.merge_history[self.history_depth.get()][1],zoom,200,50*zoom)
-
 
     def draw_piece(self,piece,zoom,x,y):
         self.canvas.create_polygon([[(p[0] + x) * zoom,p[1] * zoom+y] for p in piece.vertexes],fill="",outline="black")
@@ -23,30 +23,37 @@ class GUI():
         for i,vert in enumerate(piece.vertexes):
             self.canvas.create_text((vert[0]+x)*zoom,vert[1]*zoom+y,text=i,font=("MS gothic", "10"),fill="blue")
 
-    def draw(self,zoom):
-        zoom = float(zoom)
+    def draw(self):
+        zoom = self.zoom_scale.get()
         self.canvas.delete(tkinter.ALL)
         x = 5
         for piece in self.problem.pieces:
             self.draw_piece(piece,zoom,x,10)
             x+=numpy.amax(piece.vertexes,axis=0)[0] + 1
         self.draw_piece(self.problem.frame,zoom,5,50*zoom)
+        self.draw_history(self.problem)
+
+    def draw_handler(self,arg):
+        """
+        GUIの為のハンドラです．
+        """
+        self.draw()
 
     def __init__(self,problem):
         self.problem = problem
         self.root = tkinter.Tk()
 
         self.zoom_scale = tkinter.Scale(self.root, label = '倍率', orient = 'h',
-               from_ = 2, to = 20, command = self.draw,resolution=0.1)
+               from_ = 2, to = 20, command = self.draw_handler,resolution=0.1)
         self.canvas = tkinter.Canvas(self.root, scrollregion=("0", "0",  "10000",  "0"),width = 1000, height = 500)
         self.canvas_scrollbar = tkinter.Scrollbar(self.root, orient = 'h', command = self.canvas.xview)
         self.canvas.configure(xscrollcommand = self.canvas_scrollbar.set)
         self.history_depth = tkinter.Scale(self.root, label = '履歴', orient = 'h',
-               from_ = 0, to = 0, command = self.draw_history)
+               from_ = 0, to = 0, command = self.draw_handler)
 
         self.zoom_scale.pack(fill="x")
         self.history_depth.pack(fill="x")
         self.canvas.pack(fill="both")
         self.canvas_scrollbar.pack(fill="x",side="bottom")
 
-        self.draw(2)
+        self.draw()
--- procon.py
+++ procon.py
@@ -16,12 +16,16 @@ import QR
 root_problem=problem.problem(*QR.read_QR())
 
 #GUIデバッグ
-TEST_next_problem=problem.problem(*QR.read_QR())
-TEST_next_problem.pieces.pop(0)
 root_problem.merge_history=[(root_problem,root_problem.pieces[0],0,1,0,1)]
+import copy
+TEST_next_problem=copy.deepcopy(root_problem)
+TEST_next_problem.pieces.pop(0)
 TEST_next_problem.merge_history=[(root_problem,root_problem.pieces[0],0,1,0,1),(TEST_next_problem,TEST_next_problem.pieces[1],0,1,0,1)]
 
 gui=GUI.GUI(root_problem)
+
+#GUIデバッグ
+gui.draw_history(TEST_next_problem)
 
 def search():
     #TODO:探索処理はここに
@@ -29,8 +33,6 @@ def search():
 searching_thread=threading.Thread(target=search) 
 searching_thread.start()
 
-#GUIデバッグ
-gui.draw_history(TEST_next_problem)
 
 gui.root.mainloop()
 depth_max=0
