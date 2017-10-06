d082ba9920287ed5a8f242be631cadca8fbf3ee3
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2017-10-06 23:26:36 +0900

    デザイン少し変更と履歴バー追加，ソースコード整理．

--- GUI.py
+++ GUI.py
@@ -2,21 +2,31 @@ import tkinter
 import numpy
 
 class GUI():
+    def draw_history(self,problem):
+        """
+        ピース結合の履歴(手順)を描画します．
+
+        problem: problem
+        """
+        self.history_depth.configure(tuple_iterator=len(problem.merge_history)-1)
+
+    def draw_piece(self,piece,zoom,x,y):
+        self.canvas.create_polygon([[(p[0] + x) * zoom,p[1] * zoom+y] for p in piece.vertexes],fill="",outline="black")
+
+        self.canvas.create_rectangle((piece.vertexes[0][0] + x) * zoom,piece.vertexes[0][1] * zoom+y,(piece.vertexes[0][0] + x) * zoom + 4,piece.vertexes[0][1] * zoom + 4+y,fill="blue")
+        self.canvas.create_rectangle((piece.vertexes[1][0] + x) * zoom,piece.vertexes[1][1] * zoom+y,(piece.vertexes[1][0] + x) * zoom + 4,piece.vertexes[1][1] * zoom + 4+y,fill="yellow")
+
+        for i,vert in enumerate(piece.vertexes):
+            self.canvas.create_text((vert[0]+x)*zoom,vert[1]*zoom+y,text=i,font=("MS gothic", "10"),fill="blue")
+
     def draw(self,zoom):
         zoom = float(zoom)
         self.canvas.delete(tkinter.ALL)
-        x = 0
+        x = 5
         for piece in self.problem.pieces:
-            self.canvas.create_polygon([[(p[0] + x) * zoom,p[1] * zoom] for p in piece.vertexes])
-
-            self.canvas.create_rectangle((piece.vertexes[0][0] + x) * zoom,piece.vertexes[0][1] * zoom,(piece.vertexes[0][0] + x) * zoom + 4,piece.vertexes[0][1] * zoom + 4,fill="blue")
-            self.canvas.create_rectangle((piece.vertexes[1][0] + x) * zoom,piece.vertexes[1][1] * zoom,(piece.vertexes[1][0] + x) * zoom + 4,piece.vertexes[1][1] * zoom + 4,fill="yellow")
-
-            for i,vert in enumerate(piece.vertexes):
-                self.canvas.create_text((vert[0]+x)*zoom,vert[1]*zoom,text=i,font=("MS gothic", "15"),fill="yellow")
-
+            self.draw_piece(piece,zoom,x,10)
             x+=numpy.amax(piece.vertexes,axis=0)[0] + 1
-        self.canvas.create_polygon([[p[0] * zoom,(p[1] + 30) * zoom] for p in self.problem.frame.vertexes])
+        self.draw_piece(self.problem.frame,zoom,5,50*zoom)
 
     def __init__(self,problem):
         self.problem = problem
@@ -28,7 +38,11 @@ class GUI():
         self.canvas_scrollbar = tkinter.Scrollbar(self.root, orient = 'h', command = self.canvas.xview)
         self.canvas.configure(xscrollcommand = self.canvas_scrollbar.set)
 
+        self.history_depth = tkinter.Scale(self.root, label = '履歴', orient = 'h',
+               from_ = 0, to = 0, command = self.draw_history)
+
         self.zoom_scale.pack(fill="x")
+        self.history_depth.pack(fill="x")
         self.canvas.pack(fill="both")
         self.canvas_scrollbar.pack(fill="x",side="bottom")
 
