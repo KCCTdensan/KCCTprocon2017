2f30597c6ce131c0068ffca4a73d52395e5d3d72
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2017-10-07 12:16:39 +0900

    test

--- procon.py
+++ procon.py
@@ -18,14 +18,15 @@ root_problem=problem.problem(*QR.read_QR())
 #GUIデバッグ
 root_problem.merge_history=[(root_problem,root_problem.pieces[0],0,1,0,1)]
 import copy
-TEST_next_problem=copy.deepcopy(root_problem)
-TEST_next_problem.pieces.pop(0)
-TEST_next_problem.merge_history=[(root_problem,root_problem.pieces[0],0,1,0,1),(TEST_next_problem,TEST_next_problem.pieces[1],0,1,0,1)]
+root_problem=copy.copy(root_problem)
+root_problem.pieces=copy.copy(root_problem.pieces)
+root_problem.pieces.pop(0)
+root_problem.merge_history.append((root_problem,root_problem.pieces[1],0,1,0,1))
 
 gui=GUI.GUI(root_problem)
 
 #GUIデバッグ
-gui.draw_history(TEST_next_problem)
+gui.draw_history(root_problem)
 
 def search():
     #TODO:探索処理はここに
