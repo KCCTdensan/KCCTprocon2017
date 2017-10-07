68135e49c865cdfda1277437b34d9debf95e9dba
kumataro4160 <r116309@g.kobe-kosen.ac.jp>
2017-10-08 03:00:12 +0900

    evaluationをデバッグ済みの最新のものに更新

--- evaluation.py
+++ evaluation.py
@@ -1,5 +1,5 @@
 class evaluation:
-    def __point_in_rect(self,pointr1,pointr2,pointp):
+    def __point_in_rect(pointr1,pointr2,pointp):
         if pointr1[0]<=pointr2[0]:
             if not((pointr1[0]<=pointp[0])and(pointp[0]<=pointr2[0])):
                 return False
@@ -14,15 +14,19 @@ class evaluation:
                 return False        
         return True
 
-    def __point_on_line(self,pointl1,pointl2,pointp):
-        if __point_in_rect(pointl1,pointl2,pointp):
+    def __point_on_line(pointl1,pointl2,pointp):
+        if((pointp[0]==pointl1[0]and pointp[1]==pointl1[1])or(pointp[0]==pointl2[0]and pointp[1]==pointl2[1])):
+            return True
+        if not evaluation.__point_in_rect(pointl1,pointl2,pointp):
             return False
-        return((pointl2[0]-pointl1[0])/(pointp[0]-pointl1[0]))==((pointl2[1]-pointl1[1])/(pointp[1]-pointl1[1]))
+        if (pointl1[0]==pointp[0]and pointl2[0]==pointp[0])or(pointl1[1]==pointp[1]and pointl2[1]==pointp[1]):
+            return True
+        return(((pointl2[0]-pointl1[0])/(pointp[0]-pointl1[0]))==((pointl2[1]-pointl1[1])/(pointp[1]-pointl1[1])))
 
-    def __linea_on_lineb(self,pointla1,pointla2,piontlb1,pointlb2):
-        return __point_on_line(pointlb1,pointlb2,pointla1)and __point_on_line(pointlb1,pointlb2,pointla2)
+    def __linea_on_lineb(pointla1,pointla2,pointlb1,pointlb2):
+        return(evaluation.__point_on_line(pointlb1,pointlb2,pointla1)and evaluation.__point_on_line(pointlb1,pointlb2,pointla2))
 
-    def calc_num_of_overlapped_lines(self,frame,piece_vertexes):
+    def calc_num_of_overlapped_lines(frame,piece_vertexes):
         """枠の各辺とピースの各辺が重なる数を返します。
 
         frame:
@@ -36,13 +40,13 @@ class evaluation:
         ret=0
         verf=len(frame)
         verp=len(piece_vertexes)
-        for i in frame:
-            for j in piece_vertexes:
-                if __linea_on_lineb(piece_vertexes[j],piece_vertexes[(j+1)%verp],frame[i],frame[(i+1)&verf]):
+        for i in range(0,verf):
+            for j in range(0,verp):
+                if evaluation.__linea_on_lineb(piece_vertexes[j],piece_vertexes[(j+1)%verp],frame[i],frame[(i+1)%verf]):
                     ret+=1
         return ret
 
-    def calc_num_of_matched_vertexes(self,frame,piece_vertexes):
+    def calc_num_of_matched_vertexes(frame,piece_vertexes):
         """枠の各頂点の座標とピースの各頂点の座標が重なる数を返します。
         
         frame:
@@ -53,9 +57,9 @@ class evaluation:
         frameとpiece_vertexesに渡す配列の座標は(x,y)で表される必要があります。
         また、piece_vertexesに渡す各頂点の座標は枠(frame)に対して正確な位置である必要があります。
         """
-        return len(set(map(tuple,self.frame.vertexes))&set(map(tuple,piece_vertexes)))
+        return len(set(map(tuple,frame))&set(map(tuple,piece_vertexes)))
 
-    def calc_eval_value(self,frame,piece_vertexes):
+    def calc_eval_value(frame,piece_vertexes):
         """枠に対するピースの位置の評価値を返します。
 
         frame:
